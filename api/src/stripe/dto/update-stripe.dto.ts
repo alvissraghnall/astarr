0b79633bda49a236207f252976f717de05918bbb
Alviss Raghnall <isiomastewart@gmail.com>
2022-12-04 23:11:34 +0100

    testing api...

--- api/src/stripe/dto/update-stripe.dto.ts
+++ api/src/stripe/dto/update-stripe.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateStripeDto } from './create-payment.dto';
+import { CreatePaymentDto } from './create-payment.dto';
 
-export class UpdateStripeDto extends PartialType(CreateStripeDto) {}
+export class UpdateStripeDto extends PartialType(CreatePaymentDto) {}
