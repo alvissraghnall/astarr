2f16a8873bb9c9e018f8fdf776ec76fa320936b3
xavier <isiomastewart@gmail.com>
2025-04-02 04:04:01 +0100

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import "./index.css";
+//import "./index.css";
 import { ToastContainer } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
 
